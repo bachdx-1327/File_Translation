b6cdea56785be655a453516ef4087f2cc898555e
bachdx-1327 <duong.xuan.bach@sun-asterisk.com>
2024-05-28 12:07:47 +0700

    .

--- deeptrans.py
+++ deeptrans.py
@@ -1,15 +1,4 @@
-from deep_translator import (GoogleTranslator,
-                             ChatGptTranslator,
-                             MicrosoftTranslator,
-                             PonsTranslator,
-                             LingueeTranslator,
-                             MyMemoryTranslator,
-                             YandexTranslator,
-                             PapagoTranslator,
-                             DeeplTranslator,
-                             QcriTranslator,
-                             single_detection,
-                             batch_detection)
+from deep_translator import GoogleTranslator
 
 def vi2ja(text):
     translated = GoogleTranslator(source='vi', target='ja').translate(text=text)
